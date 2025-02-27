c764450a06d3afef3407264f65452b24d1ad705c
Zidany Palomino <zidanypalomino2@gmail.com>
2025-02-27 01:04:49 -0500

    Cambios en el archivo, implementacion de operadores aritmeticos con condicionales y funciones

--- condicionales.py
+++ condicionales.py
@@ -29,12 +29,50 @@ print("-------------------------------------------------------------------")
 #ejemplo2 de condicionales o sentencias de control
 
 if "Juan" not in amigos:
-    print("Juan no es tu amigo")
-elif "Juan" in amigos:
-    print("Juan es tu amigo")
+    print("Juan se filtro en tu lista de amigos")
+elif "Juan"  in enemigos:
+    print("Juan es tu enemigo")
 else:
     print(individuo," no conoce a Juan")
 
-''' En este caso si Juan no esta en el array de amigos, entonces se ejecutara el primer print,
-si Juan esta en el array de amigos entonces se ejecutara el segundo print, de lo contrario se ejecutara
-el tercer print que indica que la variable individuo no conoce a Juan.'''
+    '''Si Juan no esta en la lista de amigos, entonces se ejecutara el primer print,
+    de lo contrario se ejecutara el segundo print, si Juan esta en la lista de enemigos,    
+    de lo contrario se ejecutara el tercer print que indica que, la variable individuo no conoce a Juan.'''
+
+print("-------------------------------------------------------------------")
+
+#ejemplo3 de condicionales o sentencias de control| uso de operadores logicos
+
+if "Dani" is  amigos[1]: #comparando si Dani es igual a amigos en la posicion 1 del array
+    print("Dani es tu amigo")   #si se cumple la condicion se ejecutara este print
+else:
+    print("Dani no es tu amigo numero 2")
+
+print("-------------------------------------------------------------------")
+
+#ejemplo4 de condicionales o sentencias de control.
+#primer uso de operadores aritmeticos y funciones.
+
+#declarando variables para ejemplo 4
+a = 20
+b = 10
+c = 5
+
+#suma de variables 
+suma = a + b 
+print("la suma de a y b es:", suma)
+
+#declaramos array
+posiblesresultados = [30,20,40,50] 
+
+print("-------------------------------------------------------------------")
+
+if suma in posiblesresultados:#
+    def divicion(): #funcion para dividir la suma de a y b
+        return suma / c #return es un metodo que nos permite retornar un valor en este caso la divicion de la suma de a y b entre c.
+    print("la divicion de la suma de a y b es:", divicion())
+else:
+    print("division no valida ya que la suma de a + b no se encuentra en posibles resultados")
+
+'''Si el valor de la suma de a +b está en el array se ejecuta la funcion de divicion q retorna la el resultado de la suma,
+dividido sobre c , caso sontrario que suma no esté en el array se ejecuta el segunto print'''
